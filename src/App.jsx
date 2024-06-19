c24c689fb6172c3c854119d0545e5bdff8012e38
anurag050301 <anurag050301@users.noreply.github.com>
2024-06-19 11:36:56 +0530

    done with the front end part

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Weather from './components/Weather'
 const App = () => {
   return (
     <div className='app'>
-      
       <Weather />
     </div>
   )
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,17 +1,38 @@
-import React from 'react'
-import './Weather.css'
-import { FaMapLocationDot,FaSun } from "react-icons/fa6";
-import GradientSunIcon from './GradientSunIcon';
+import React from "react";
+import "./Weather.css";
+import { FaMapLocationDot, FaWind } from "react-icons/fa6";
+import { MdSunny } from "react-icons/md";
+import { WiHumidity } from "react-icons/wi";
 const Weather = () => {
   return (
-    <div className='weather'>
-        <div className='search-bar'>
-            <input type='text' placeholder='Search'/>
-            <FaMapLocationDot class="custom-icon" />
+    <div className="weather">
+      <div className="search-bar">
+        <input type="text" placeholder="Search" />
+        <FaMapLocationDot class="custom-icon" />
+      </div>
+      <div className="icon-wrapper">
+        <MdSunny className="gradient-sunny" />
+      </div>
+      <p className="temperature">46°C</p>
+      <p className="location">Ghaziabad</p>
+      <div className="weather-data">
+        <div className="col">
+          <WiHumidity className="show-logo" />
+          <div>
+            <p>91%</p>
+            <span>Humidity</span>
+          </div>
         </div>
-        <GradientSunIcon />
+        <div className="col">
+          <FaWind className="show-logo"/>
+          <div>
+            <p>3.6 Km/h</p>
+            <span>Wind Speed</span>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Weather
+export default Weather;
